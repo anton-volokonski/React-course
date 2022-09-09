4fb4c48876286ebdce683511b5044f04b0e0be5e
Anton Volokonski <anton.volo@gmail.com>
2022-09-09 16:53:03 +0300

    update react tutorial app

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,49 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
+import './index.css'
 
-function Greeting() {
-    return <h4>Yoyoyo</h4>
+function BookList() {
+    return (
+        <section className='block-list'>
+            {books.map((obj) => {
+                return <Book key={obj.id} book={obj} />
+            })}
+        </section>
+    )
 }
 
-ReactDOM.render(<Greeting />, document.getElementById('root'))
+const Book = (props) => {
+    const {imgLink, title, description} = props.book
+    return (
+        <article className='block-item'>
+            <img alt='' src={imgLink} />
+            <h4 style={{color:'chocolate', marginTop:'0.25rem'}}>{title}</h4>
+            <p>{description}</p>
+        </article>
+    )
+}
+
+const books = [
+    {
+        id: 1,
+        imgLink: 'https://www.apple.com/ru/safari/images/overview/privacy_hero__fn3khpbm736u_small_2x.png',
+        title: 'iMac',
+        description: 'very good device'
+    },
+    {
+        id: 2,
+        imgLink: 'https://www.apple.com/ru/safari/images/overview/performance_hero__b0mm4zgtpeqa_small_2x.jpg',
+        title: 'Macbook',
+        description: 'must have one'
+    },
+    {
+        id: 3,
+        imgLink: 'https://files.foxtrot.com.ua/PhotoNew/img_0_82_85_1.jpg',
+        title: 'Grill',
+        description: 'not a laptop'
+    }
+]
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<BookList />);
